f43c169ef4273bf91f73a8fc97abb1d1a98f06c7
atluo04 <54228854+atluo04@users.noreply.github.com>
2024-03-03 18:35:57 -0800

    lbby screen stuff

--- TagConnect-frontend/screens/LobbyScreen.js
+++ TagConnect-frontend/screens/LobbyScreen.js
@@ -18,7 +18,7 @@ import {
   Outfit_600SemiBold,
 } from "@expo-google-fonts/dev";
 
-const LobbyScreen = ({ navigation }) => {
+const LobbyScreen = ({ navigation, currentLobbyID }) => {
   let [fontsLoaded, fontError] = useFonts({
     Righteous_400Regular,
     Outfit_400Regular,
@@ -26,10 +26,47 @@ const LobbyScreen = ({ navigation }) => {
     Outfit_600SemiBold,
   });
   const [currentLocation, setCurrentLocation] = useState(null);
-  const [lobbyID, setLobbyID] = useState(null);
+  const [lobbyID, setLobbyID] = useState(currentLobbyID);
+  const [users, setUsers] = useState([]);
 
-  
-  
+  async function fetchData() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      alert("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(location.coords);
+    setCurrentLocation(location.coords);
+
+    try {
+      const response = await fetch(
+        `${process.env.EXPO_PUBLIC_BACKEND_SERVER}/get-users-in-lobby/${lobbyID}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            lobbyId : lobbyID
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.status == 200) {
+        setLobbyID(data.lobby);
+      } else {
+        alert("Error finding users!");
+      }
+    } catch (error) {
+      alert("Server error!");
+      console.log(error);
+    }
+  }
+
 };
 
 export default LobbyScreen;
